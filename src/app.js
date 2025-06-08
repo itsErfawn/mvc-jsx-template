d853379cce26ade1896c267462892b1246439c67
ErFaN <safarierfan29@gmail.com>
2025-06-08 12:31:54 +0330

    update app.js

--- src/app.js
+++ src/app.js
@@ -6,17 +6,14 @@ require('babel-register')({
 
 const app = express();
 
-// تنظیمات View Engine برای JSX
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 8888;
